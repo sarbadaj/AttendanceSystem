b3c121c7706be29b3fa331cd34e73097798580de
sarbadaj <sarbadaj@yahoo.com>
2015-01-05 14:21:24 +0545

    added a name column to company and established association between user and company

--- db/migrate/20150105081756_create_companies.rb
+++ db/migrate/20150105081756_create_companies.rb
@@ -1,6 +1,7 @@
 class CreateCompanies < ActiveRecord::Migration
   def change
     create_table :companies do |t|
+      t.string :name, null: false, default: ""
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150105073659) do
+ActiveRecord::Schema.define(version: 20150105081756) do
+
+  create_table "companies", force: true do |t|
+    t.string   "name",       default: "", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",   null: false
     t.string   "encrypted_password",     default: "",   null: false
+    t.integer  "user_id"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -31,5 +38,6 @@ ActiveRecord::Schema.define(version: 20150105073659) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["user_id"], name: "index_users_on_user_id", using: :btree
 
 end

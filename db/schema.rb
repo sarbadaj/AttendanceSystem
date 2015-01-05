637fbed8791368e0959b3051190aa4cdb65f6aa9
sarbadaj <sarbadaj@yahoo.com>
2015-01-05 15:30:37 +0545

    added company id column to the users table

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20150105081756) do
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",   null: false
     t.string   "encrypted_password",     default: "",   null: false
-    t.integer  "user_id"
+    t.integer  "company_id"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -36,8 +36,8 @@ ActiveRecord::Schema.define(version: 20150105081756) do
     t.boolean  "admin",                  default: true
   end
 
+  add_index "users", ["company_id"], name: "index_users_on_company_id", using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["user_id"], name: "index_users_on_user_id", using: :btree
 
 end

9cfc67619ee6aac78e52de98a290f02317e3bad1
sarbadaj <sarbadaj@yahoo.com>
2015-01-05 15:27:18 +0545

    defined routes of users and companies

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users, :controllers => { :invitations => 'users/invitations', 
+                                      :registrations => 'users/registrations',
+                                      :sessions => "users/sessions"
+                                    }
+
+  devise_scope :user do
+    root to: "users/sessions#new"
+  end 
+
+  resources :companies
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

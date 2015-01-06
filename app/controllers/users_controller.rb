ea429dfbb332e018a5de79d6ab38196746d70cba
sarbadaj <sarbadaj@yahoo.com>
2015-01-07 00:11:17 +0545

    modified to change user role

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,37 @@
 class UsersController < ApplicationController
-	def index
-		@users = current_user.company.users
-	end
+
+  def new
+    @user = User.new
+  end
+
+  def index
+    @users = current_user.company.users
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.admin = params[:user][:admin]
+    if @user.save
+      flash[:notice] = "Role of #{@user.email} has been changed."
+      redirect_to users_path
+    else
+      flash[:notice] = "Role of #{@user.email} has not been changed."
+      redirect_to users_path
+    end
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+   
+    redirect_to users_path
+  end
 end

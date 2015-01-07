d2d4a6a714acc161d8364bc35418a2cd57a16cd5
sarbadaj <sarbadaj@yahoo.com>
2015-01-07 11:39:07 +0545

    created action to save login time and logout time

--- app/controllers/timetracks_controller.rb
+++ app/controllers/timetracks_controller.rb
@@ -1,2 +1,20 @@
 class TimetracksController < ApplicationController
+
+  def index
+    @timetracks = Timetrack.all
+  end
+
+  def login
+    @timetrack = current_user.timetracks.new
+    @timetrack.login_time = Time.now
+    @timetrack.save
+    redirect_to companies_path
+  end
+
+  def logout
+    @timetrack = current_user.timetracks.sort_by{|t| t.id}.last
+    @timetrack.logout_time = Time.now
+    @timetrack.save
+    redirect_to companies_path
+  end
 end

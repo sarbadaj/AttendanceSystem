16310316bfe34f965093cecf5b4c1446aeeb5066
sarbadaj <sarbadaj@yahoo.com>
2015-01-07 15:16:40 +0545

    passing total users in companies index

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,6 +2,7 @@ class CompaniesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @users = current_user.company.users
     @comapany = current_user.company
   end
 end

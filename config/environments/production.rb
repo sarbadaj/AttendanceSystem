84bb9586ca2e6f1043e104600fa222d24cb7b85f
sarbadaj <sarbadaj@yahoo.com>
2015-03-21 07:54:48 +0545

    modified for mailer in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'testtalkruby@gmail.com',
+    password:             'rubyrubyruby',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
